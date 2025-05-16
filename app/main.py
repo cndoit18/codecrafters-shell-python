ce70c217bcecd9eee3731e21b0293af66ac1a4ef
cndoit18 <cndoit18@outlook.com>
2025-05-16 13:55:31 +0800

    feat: add builtin command handler with type command support
    Change-Id: Ibd19555c32cb1d10bcb7368ea6aba3d874ae4e7f

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 import sys
 
+builtin = {
+    "exit": lambda *code: sys.exit(int(code[0]) if len(code) > 0 else 0),
+    "echo": lambda *args: print(" ".join(args)),
+}
+
 
 def main():
     while True:
@@ -8,9 +13,14 @@ def main():
         command = input()
         match command.split():
             case "exit", *code:
-                sys.exit(int(code[0]) if len(code) > 0 else 0)
+                builtin["exit"](*code)
             case "echo", *args:
-                print(" ".join(args))
+                builtin["echo"](*args)
+            case "type", cmd:
+                if cmd in builtin or cmd == "type":
+                    print(f"{cmd} is a shell builtin")
+                else:
+                    print(f"{cmd}: not found")
             case _:
                 print(f"{command}: command not found")
 
