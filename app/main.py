94c06d8166d66c8d064c53ce26cc3093cddeef7e
cndoit18 <cndoit18@outlook.com>
2025-05-16 13:43:20 +0800

    feat: add echo command handler
    Change-Id: Ib7bf4eb88cf67b53347bfc7a5f3e20484d88d350

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ def main():
         match command.split():
             case "exit", *code:
                 sys.exit(int(code[0]) if len(code) > 0 else 0)
+            case "echo", *args:
+                print(" ".join(args))
             case _:
                 print(f"{command}: command not found")
 
