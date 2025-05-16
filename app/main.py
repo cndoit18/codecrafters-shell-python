bd67e2594df27f41efaf1d4bba9ad40af2ac44a2
cndoit18 <cndoit18@outlook.com>
2025-05-16 13:40:15 +0800

    feat: add exit command with optional status code support
    Change-Id: I523a753397e7a3c394d04087e1ef4b2b807b1c72

--- app/main.py
+++ app/main.py
@@ -6,7 +6,11 @@ def main():
         sys.stdout.write("$ ")
 
         command = input()
-        print(f"{command}: command not found")
+        match command.split():
+            case "exit", *code:
+                sys.exit(int(code[0]) if len(code) > 0 else 0)
+            case _:
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
